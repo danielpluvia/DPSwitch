d6c88242826599ae57d754a1d79af761d0048129
Daniel <danielpluvia@outlook.com>
2017-11-20 15:57:45 +1100

    Added a Xcode project

---
+++ SwitchFramework/SwitchFramework/SwitchFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwitchFramework.h
+//  SwitchFramework
+//
+//  Created by Xueqiang Ma on 20/11/17.
+//  Copyright © 2017 Daniel Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwitchFramework.
+FOUNDATION_EXPORT double SwitchFrameworkVersionNumber;
+
+//! Project version string for SwitchFramework.
+FOUNDATION_EXPORT const unsigned char SwitchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwitchFramework/PublicHeader.h>
+
+
